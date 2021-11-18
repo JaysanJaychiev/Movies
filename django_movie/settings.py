a3daa290464e539c0513b5b26e257757269a2615
JaysanJaychiev <jaysan.jaychiev.11@gmail.com>
2021-11-18 21:26:03 +0600

    sekret token

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -139,8 +139,8 @@ LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'jaysan.jaychiev.kk@gmail.com'
-EMAIL_HOST_PASSWORD = 'jaysan123321'
+EMAIL_HOST_USER = os.environ.get('GMAIL_LOGIN')
+EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASSWORD')
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
